54bea788f9014283b9a4359b36533b6577a057f2
Paweł Beder <pawel.beder@gmail.com>
2019-01-13 16:45:20 +0100

    Initial Spring Initializer project.

---
+++ src/main/java/pl/beder/coreservices/CoreServicesApplication.java
@@ -0,0 +1,14 @@
+package pl.beder.coreservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreServicesApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/pl/beder/coreservices/CoreServicesApplicationTests.java
@@ -0,0 +1,17 @@
+package pl.beder.coreservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+

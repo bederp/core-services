9939045652b967aba302c68b8a59ff4e4ca35a3f
Paweł Beder <pawel.beder@gmail.com>
2019-01-13 17:58:23 +0100

    google_checkstyle + reformating code.

--- src/main/java/pl/beder/coreservices/CoreServicesApplication.java
+++ src/main/java/pl/beder/coreservices/CoreServicesApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreServicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreServicesApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CoreServicesApplication.class, args);
+  }
 }
-
--- src/main/java/pl/beder/coreservices/domain/Request.java
+++ src/main/java/pl/beder/coreservices/domain/Request.java
@@ -4,9 +4,9 @@ import lombok.Data;
 
 @Data
 public class Request {
-    final String clientId;
-    final long requestId;
-    final String name;
-    final int quantity;
-    final double price;
+  private final String clientId;
+  private final long requestId;
+  private final String name;
+  private final int quantity;
+  private final double price;
 }
